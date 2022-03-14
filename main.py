e432bf17be71ba5e404623b16225999dcd43e71a
rte <90905804+dinhngoctuyen4125@users.noreply.github.com>
2022-03-14 12:50:50 +0700

    run deadline

---
+++ main.py
@@ -0,0 +1,10 @@
+from pytube import YouTube
+import os
+
+urls_inp = open('main.inp', 'r')
+path = input('Enter path: ') or os.path.join(os.path.expanduser('~'), 'Downloads')
+
+for url in urls_inp.readlines():
+    my_video = YouTube(url)
+    my_video.streams.get_highest_resolution().download(path)
+    print(my_video.title, '- **download successfully!**')
