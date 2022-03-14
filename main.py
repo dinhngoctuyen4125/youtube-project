3c221910761f0ec84cb2338d69ee99002f53df3d
rte <90905804+dinhngoctuyen4125@users.noreply.github.com>
2022-03-14 12:55:34 +0700

    update thang nao nhap lao url thi d download dc

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ urls_inp = open('main.inp', 'r')
 path = input('Enter path: ') or os.path.join(os.path.expanduser('~'), 'Downloads')
 
 for url in urls_inp.readlines():
-    my_video = YouTube(url)
-    my_video.streams.get_highest_resolution().download(path)
-    print(my_video.title, '- **download successfully!**')
+    try:
+        my_video = YouTube(url)
+        my_video.streams.get_highest_resolution().download(path)
+        print(my_video.title, '- **download successfully!**')
+    except:
+        print('- Cannot find video!')
